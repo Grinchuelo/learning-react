e1d19695f7884de569e00bdeff50a238430afdf0
Grin <santinogreen14@gmail.com>
2025-06-19 17:07:09 -0300

    feat(04): debounce is now optimized using the just-debounce-it dependency from github.com/angus-c/just

--- projects/04-buscador-peliculas/src/hooks/useSearch.js
+++ projects/04-buscador-peliculas/src/hooks/useSearch.js
@@ -1,19 +1,18 @@
 import { useEffect, useState, useRef, useCallback } from "react"
+import debounce from "just-debounce-it"
 
 export function useSearch() {
   const [query, setQuery] = useState('')
   const [error, setError] = useState(null)
   const ref = useRef(true)
   let isFirstInput = ref.current
-  let timeOutID
 
   const debouncedGetMovies = useCallback(
-    (getMovies, newQuery) => {
-      clearTimeout(timeOutID)
-      timeOutID = setTimeout(() => {
+    debounce((getMovies, newQuery) => {
         getMovies(newQuery)
-      }, 600)
-    }, [])
+    }, 600), []
+  )
+    
 
   const updateQuery = (e, getMovies) => {
     const newQuery = e.target.value
